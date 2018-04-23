cfe837c1b0818e0ed0af8187fb89528e679a9a29
STUDENTS\1796146 <chrdalian56@gmail.com>
2018-04-23 12:01:21 -0400

    merged project

---
+++ PhpBased/config.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Used to store website configuration information.
+ *
+ * @var string
+ */
+function config($key = '')
+{
+    $config = [
+        'name' => 'Kitty Project',
+        'nav_menu' => [
+            '' => 'Home',
+            'about-us' => 'About Us',
+            'products' => 'Our Cats',
+            'contact' => 'Contact',
+        ],
+        'template_path' => 'template',
+        'content_path' => 'content',
+        'pretty_uri' => true,
+        'version' => 'v2.0',
+    ];
+
+    return isset($config[$key]) ? $config[$key] : null;
+}
