8d5ed30bba278b007c3928923e919e6df689657a
CD <CD@CDD.CO>
2018-04-25 15:25:18 -0400

    Work on adopted page

--- Project_All_connected/content/adopted.php
+++ Project_All_connected/content/adopted.php
@@ -13,7 +13,7 @@
         display: block;
         max-width: 100%;
         height: auto;
-        
+
     }
 
     .gallery-container h1 {
@@ -32,6 +32,7 @@
 
     .tz-gallery {
         padding: 40px;
+        margin-top: 5%;
     }
 
     .tz-gallery .thumbnail {
@@ -53,13 +54,13 @@
         border-radius: 4px 4px 0 0;
         height: 250px;
         min-width: 250px;
-        width: auto;
+        width: 100%;
         -webkit-box-align: center;
-      -ms-flex-align: center;
-          align-items: center;
-  -webkit-box-pack: center;
-      -ms-flex-pack: center;
-          justify-content: center;
+        -ms-flex-align: center;
+        align-items: center;
+        -webkit-box-pack: center;
+        -ms-flex-pack: center;
+        justify-content: center;
     }
 
     .tz-gallery .caption {
@@ -80,51 +81,35 @@
         margin: 0;
     }
 
-
-    /*
-    .gallery {
-        text-align: center;
-        width: 60%;
-        margin: 0 auto;
-    }
-    .gallery:after {
-        content: '';
-        display: block;
-        height: 2px;
-        margin: .5em 0 1.4em;
-        background-image: -webkit-linear-gradient(left, rgba(0, 0, 0, 0) 0%, rgba(77,77,77,1) 50%, rgba(0, 0, 0, 0) 100%);
-        background-image: linear-gradient(to right, rgba(0, 0, 0, 0) 0%, rgba(77,77,77,1) 50%, rgba(0, 0, 0, 0) 100%);
-    }
-
-    .gallery img {
-        height: 100%;
-    }
-
-    .gallery a {
-        width: 240px;
-        height: 180px;
-        display: inline-block;
-        overflow: hidden;
-        margin: 4px 6px;
-        box-shadow: 0 0 4px -1px #000;
-    }
-
-    .caption {
-        border: 1px solid black;
-    }
-
-    .thumbnail {
-        border: 1px solid black;
-    }
-    */
     .adoptedSpace{
-    margin-bottom: 10px;
-}
+        margin-bottom: 10px;
+    }
+
+    .aboutEven {
+        margin-top: 2%;
+        padding: 4%;
+    }
+
+    .modal {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        z-index: 10040;
+        overflow: auto;
+        overflow-y: auto;       
+    }
+
 </style>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 
 <link rel="stylesheet" type="text/css" href="styles/adopted.css"/>
 
-<div class=" aboutEven aboutMain notMoving">
+
+
+<div class="aboutEven aboutMain notMoving">
     <h1 class="textCenter">Our cats with new homes</h1>
 </div>
 
@@ -137,14 +122,18 @@
             <div class="col-sm-6 col-md-4">
 
                 <div class='thumbnail'>
-                    <a href="images/_DSC3450.jpg" data-caption="Image caption" caption='test' class='lightbox'>
+                    <a href="images/_DSC3450.jpg" data-caption="Image caption" class='lightbox'>
                         <img src="images/_DSC3450.jpg" alt="First image">
+
                     </a>
                     <div class="caption">
-                        <h3>"The bear"</h3>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#theBearModalCenter">
+                            "The Bear"
+                        </button>
                         <!-- <button type="button" class="btn btn-primary">More Info</button> -->
 
                     </div>
+
                 </div>
             </div>
 
@@ -171,38 +160,23 @@
                     </div>
                 </div>
             </div>
-        </div>
-          
-            <!--<div class="col-sm-6 col-md-4" class='lightbox'>
-                <div class='thumbnail'>
-                    <a href="_DSC3455.jpg" data-caption="Image caption" caption='test'>
-                        <img src="_DSC3455.jpg" alt="First image">
-                    </a>
-                    <div class="caption">
-                        <h3>Kitty 4</h3>
-                        <p></p>
-                    </div>-->
-                </div>
-            
-            
-        
-         <!--end first row-->
-      
-          <!--second row-->
-          <div class="tz-gallery">
-       <div class="row row-eq-height adoptedSpace">
+        </div>                          
+        <!--end first row-->
+
+        <!--second row-->
+        <div class="row row-eq-height adoptedSpace">
 
             <div class="col-sm-6 col-md-4">
 
                 <div class='thumbnail'>
-                    <a href="images/_DSC3965.jpg" data-caption="Image caption" caption='test' class='lightbox'>
+                    <a href="images/_DSC3965.jpg" data-caption="Image caption" class='lightbox'>
                         <img src="images/_DSC3965.jpg" alt="First image">
                     </a>
                     <div class="caption">
                         <h3>"The bear 2"</h3>
                         <!-- <button type="button" class="btn btn-primary">More Info</button> -->
 
-                </div>
+                    </div>
                 </div>
             </div>
 
@@ -230,12 +204,10 @@
                 </div>
             </div>
         </div>
-          </div>
-           <!--end second row-->
-           
-            <!--third row-->
-            <div class="tz-gallery">
-    <div class="row row-eq-height adoptedSpace">
+        <!--end second row-->
+
+        <!--third row-->
+        <div class="row row-eq-height adoptedSpace">
 
             <div class="col-sm-6 col-md-4">
 
@@ -247,7 +219,7 @@
                         <h3>"Boubou"</h3>
                         <!-- <button type="button" class="btn btn-primary">More Info</button> -->
 
-                   </div>
+                    </div>
                 </div>
             </div>
 
@@ -275,10 +247,42 @@
                 </div>
             </div>
         </div>
-            </div>
-             <!--end third row-->
-
     </div>
+    <!--end third row-->
+
+    <!-- Button trigger modal -->
+
+
+    <!-- Modals -->
+    
+    !-- "The Bear" Modal -->
+    <div class="modal fade" id="theBearModalCenter" tabindex="-1" role="dialog" aria-labelledby="theBearModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="theBearModalLongTitle">"The Bear"</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <h6>Breed: Cat</h6><br />
+                    <h6>Age: 99</h6><br />
+                    <h6>Gender: Male</h6><br />
+                    <h6>Vaccinated: Yes</h6><br />
+                    <h6>De-Clawed: No</h6><br />
+                    <h6>About "The Bear"</h6>
+                    <p>We call him "The Bear" because he eats people and he weights
+                        800 pounds.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Adopt!</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <script>
@@ -288,4 +292,5 @@
         animation: 'slideIn',
         buttons: true
     });
+
 </script>
