7c8c7703c9fc5ac992e2a1bccf720af607018cac
CD <CD@CDD.CO>
2018-04-27 21:06:45 -0400

    Added some functions to gallery
    Using ajax so user can send a message if he wants to adopt

--- Project_All_connected/content/gallery.php
+++ Project_All_connected/content/gallery.php
@@ -1,26 +1,70 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 
-<link rel="stylesheet" type="text/css" href="styles/adopted.css"/>
 
-<div class="aboutEven aboutMain notMoving">
-    <h1 class="textCenter">These kitties need a new home!</h1>
+<link rel="stylesheet" type="text/css" href="styles/adopt.css"/>
+
+
+<div class="aboutEven aboutMain pageBanner">
+    <h1 class="textCenter">Find your perfect companion</h1>
 </div>
 
+
+
 <div id="container" class="container gallery-container">
+
+    <div class="mainHeader">
+        <h2>Our Featured Cats and Kittens</h2>
+    </div>
+
     <div class="tz-gallery">
         <div class="row row-eq-height adoptedSpace" id='row'>
 
+            <script>
+                $(document).ready(function () {
+                    $(".btnAdopt").click(function () {
+                        $(".modal").modal('hide');
+                        $("#adoptModal").modal('show');
+                    });
+
+                    // using ajax to send the message to the server without reloading or redirecting page
+                    $('#btnAdoptRequest').click(function () {
+
+                        var userName = $("#userName").val();
+                        var message = $("#message").val();
+
+
+                        $.ajax({
+                            url: 'adoptInfoRequest.php',
+                            type: 'POST',
+                            data: {
+                                userName: userName,
+                                message: message
+                            },
+                            success: function (msg) {
+                                $("#adoptModal").modal('hide');
+
+                                alert("Your message was sent!");
+
+                            }
+                        });
+                    });
+                });
+            </script>
 
 
         </div>
     </div>
 </div>
+
+
+
 <?php
 
+// php scripts below
 function getConnection() {
     $servername = "den1.mysql2.gear.host";
     $username = "phpcats";
@@ -44,7 +88,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
 
     while ($row = $result->fetch_assoc()) {
-        
+
         // if the availability date is 0000-00-00, consider the cat adopted and dont display it
         $availDate = $row['availabilityDate'];
 
@@ -58,7 +102,7 @@ if ($result->num_rows > 0) {
         $vaccineA = ((bool) $row['vaccineA']) ? 'yes' : 'no';
         $vaccineB = ((bool) $row['vaccineB']) ? 'yes' : 'no';
         $deworming = ((bool) $row['deworming']) ? 'yes' : 'no';
-        
+
         ($row['gender'] === null) ? $gender = 'Unknown' : $gender = $row['gender'];
         ($row['description'] === null) ? $description = 'No description' : $description = row['description'];
 
@@ -72,6 +116,7 @@ if ($result->num_rows > 0) {
         addThumbnail($name, $imageLink);
         createModal($name, $dob, $gender, $vaccineA, $vaccineB, $deworming, $availDate, $description);
     }
+    createAdoptModal(); // the modal for when you click "adopt", only 1 is needed
 } else {
     echo "0 results";
 }
@@ -150,9 +195,49 @@ function createModal($name, $dob, $gender, $vaccineA, $vaccineB, $deworming, $av
                 </div>
                 <div class='modal-footer'>
                     <button type='button' class='btn btnAll' data-dismiss='modal'>Close</button>
-                    <button type='button' class='btn btnAll'>Adopt " . $name . "!</button>
+                    <button type='button' class='btn btnAll btnAdopt'>Adopt " . $name . "!</button>
                 </div>
             </div>
         </div>
     </div>";
+}
+
+function createAdoptModal() {
+    echo "
+        <div class='modal fade' id='adoptModal' tabindex='-1' role='dialog' aria-labelledby='adoptModalLabel' aria-hidden='true'>
+        
+            <div class='modal-dialog modal-dialog-centered' role='document'>
+            
+                <div class='modal-content'>
+                
+                  <div class='modal-header'>
+                    <h4 class='modal-title' id='exampleModalLabel'>Ready to adopt?</h5>
+                    <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+                      <span aria-hidden='true'>&times;</span>
+                    </button>
+                  </div>
+                  
+                  <div class='modal-body'>
+                    <button type='button' class='btn btnAll btnSuccess btnCall'><span class='glyphicon glyphicon-earphone'</span>
+                        Call us at 1-800-1212-0155</button>
+                    <h5>Or send us a direct message, we'll get back to you asap!</h5>
+                    <form>
+                      <div class='form-group'>
+                        <label for='userName' class='col-form-label'>Your name:</label>
+                        <input type='text' class='form-control' id='userName' name='userName'>
+                      </div>
+                      <div class='form-group'>
+                        <label for='message-text' class='col-form-label'>Your Message: (make sure you leave us a way to contact you!)</label>
+                        <textarea class='form-control' id='message'></textarea>
+                      </div>
+                    </form>
+                  </div>
+                  
+                  <div class='modal-footer'>
+                    <button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>
+                    <button type='button' class='btn btn-primary' id='btnAdoptRequest'>Send message</button>
+                  </div>                 
+            </div>
+          </div>
+        </div>";
 }
