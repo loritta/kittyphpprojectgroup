65fe6be3d62cdcb44a096218c8fae3e5b3a12b12
Lara <larisa.sabalin@gmail.com>
2018-04-29 23:58:07 -0400

    resources

--- Project_All_connected/content/resources.php
+++ Project_All_connected/content/resources.php
@@ -5,3 +5,60 @@
  * and open the template in the editor.
  */
 ?>
+<link rel="stylesheet" href="styles/about.css" />
+
+<div class="resourcesMain aboutEven">
+    <div class="aboutText">
+        <br>
+        <br>
+        <h1>Useful resources</h1>
+        <br>
+    </div>
+</div>
+
+<div class="galleryBatch aboutOdd">
+    </br> 
+    <br>
+
+    <h2 class="textCenter">Books</h2>
+    <div class="resContainer resCenter">
+    <p class="">Scottish Fold Cats: 
+        Everything About Acquisition, Care, Nutrition, Behavior, Health Care, and Breeding 
+        (Complete Pet Owner's Manual)
+    </p>
+    <a class="resCenter"
+       href='https://www.amazon.com/Scottish-Fold-Cats-Everything-Acquisition/dp/0812049993' style='color:blue;'>Click here</a>
+    <p class="">Complete Kitten Care 
+
+    </p>
+    <a class="resCenter"
+       href='https://www.amazon.com/dp/B003I851O6/ref=sspa_dk_detail_0?psc=1&pd_rd_i=B003I851O6&pd_rd_wg=p9hN4&pd_rd_r=E4ASHDHZYA7YEYY60WN2&pd_rd_w=ReNup' style='color:blue;'>Click here</a>
+    <p class="">The Cat Owner's Manual: Operating Instructions, Troubleshooting Tips, and Advice on Lifetime Maintenance (Owner's and Instruction Manual)  
+
+    </p>
+    <a class="resCenter"
+       href='https://www.amazon.com/Cat-Owners-Manual-Instructions-Troubleshooting-ebook/dp/B00JNPF54E/ref=pd_sim_351_2?_encoding=UTF8&psc=1&refRID=SYAVBPFJKZ4AQB9EAKAG' style='color:blue;'>Click here</a>
+</div>
+</div>
+<div class="resourcesPhoto"></div>
+<div class="galleryBatch aboutEven">
+
+    <br>
+    <br>    
+    <h2 class="textCenter">Pet centers</h2> 
+     <div class="resContainer resCenter">
+    <p class="">Mondou</p>
+    <a class="resCenter"
+        href='https://www.mondou.com/en-CA/' style='color:blue;'>Click here</a>
+    <p class="">Bailey Blu Animalerie 
+       
+    </p>
+     <a class="resCenter"
+         href='http://www.baileyblu.com' style='color:blue;'>Click here</a>
+    <p class="">Animal Nature Center 
+        
+    </p>
+    <a class="resCenter"
+        href='http://www.naturepet.com' style='color:blue;'>Click here</a>
+</div>
+</div>
